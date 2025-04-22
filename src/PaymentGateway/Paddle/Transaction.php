72bb6ed86622d598325e806d55c3b97733492514
Felipe Santos <felipepmsv@gmail.com>
2025-04-22 14:44:15 -0300

    2.4 - PHP Namespace Tutorial

---
+++ src/PaymentGateway/Paddle/Transaction.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PaymentGateway\Paddle;
+
+use Notification\Email;
+
+class Transaction
+{
+    public function __construct() 
+    {
+        var_dump(new Email());
+        //var_dump(new CustomerProfile());
+    }   
+ 
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,10 @@
 <?php
 
-// Muitas putariazinhas para descrever aqui
-// melhor consultar a documentacao quando for necessario
+require_once '../PaymentGateway/Stripe/Transaction.php';
+require_once '../PaymentGateway/Paddle/Transaction.php';
+require_once '../PaymentGateway/Paddle/CustomerProfile.php';
+require_once '../Notification/Email.php';
+
+use PaymentGateway\Paddle\Transaction;
+
+var_dump(new Transaction());
