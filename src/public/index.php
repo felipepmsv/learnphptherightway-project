d795a3334e09e654308cf76b7d855fbb5a142ba6
Felipe Santos <felipepmsv@gmail.com>
2025-05-21 15:51:56 -0300

    2.5 - Autoloading

--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,11 @@
 <?php
 
-require_once '../app/PaymentGateway/Stripe/Transaction.php';
-require_once '../app/PaymentGateway/Paddle/Transaction.php';
-require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
-require_once '../app/Notification/Email.php';
+spl_autoload_register(function($class)
+{
+    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
+
+    require $path;    
+});
 
 use App\PaymentGateway\Paddle\Transaction;
 
