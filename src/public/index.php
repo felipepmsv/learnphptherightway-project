646905c28987d4a880c4f2ef23d00c121c31ec05
Felipe Santos <felipepmsv@gmail.com>
2025-05-21 17:07:14 -0300

    2.5 - Composer

--- src/public/index.php
+++ src/public/index.php
@@ -1,22 +1,11 @@
 <?php
 
-// PSR-4: Autoloader
-// https://www.php-fig.org/psr/psr-4/
-
-spl_autoload_register(function($class)
-{
-    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
-
-    // 2.4 - Autoloader implementations MUST NOT throw exceptions, 
-    // MUST NOT raise errors of any level, and SHOULD NOT return a value.
-    if (file_exists($path)) {
-        require $path;
-    }
-    
-});
+require __DIR__ . '/../vendor/autoload.php';
 
 use App\PaymentGateway\Paddle\Transaction;
 
-$paddleTransaction = new Transaction();
+//$paddleTransaction = new Transaction();
 
-var_dump($paddleTransaction);
+$id = new \Ramsey\Uuid\UuidFactory();
+
+echo $id->uuid4();
