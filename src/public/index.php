30c7ba71be06cdad9f37fa1a9db5a67abf243fab
Felipe Santos <felipepmsv@gmail.com>
2025-05-21 16:19:38 -0300

    2.5 - PHP Coding Standards, Autoloading (PSR-4)

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,18 @@
 <?php
 
+// PSR-4: Autoloader
+// https://www.php-fig.org/psr/psr-4/
+
 spl_autoload_register(function($class)
 {
     $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
 
-    require $path;    
+    // 2.4 - Autoloader implementations MUST NOT throw exceptions, 
+    // MUST NOT raise errors of any level, and SHOULD NOT return a value.
+    if (file_exists($path)) {
+        require $path;
+    }
+    
 });
 
 use App\PaymentGateway\Paddle\Transaction;
