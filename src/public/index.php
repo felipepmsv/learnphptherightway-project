fcd5e245d0a8c6a0e300d7f5abc3aa95a367f05b
Felipe Santos <felipepmsv@gmail.com>
2025-05-22 16:12:36 -0300

    2.5 - PHP Coding Standards, Autoloading (PSR-4) & Composer

--- src/public/index.php
+++ src/public/index.php
@@ -4,8 +4,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\PaymentGateway\Paddle\Transaction;
 
-//$paddleTransaction = new Transaction();
+$paddleTransaction = new Transaction();
 
 $id = new \Ramsey\Uuid\UuidFactory();
 
 echo $id->uuid4();
+
+var_dump($paddleTransaction);
