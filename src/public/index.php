e9b28a49ed9e348784b9ccf1a1bd956d4f8c8331
Felipe Santos <felipepmsv@gmail.com>
2025-05-22 17:23:53 -0300

    2.6 - Object Oriented PHP - Class Constants

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Enums\Status;
 use App\PaymentGateway\Paddle\Transaction;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $transaction = new Transaction();
 
-$transaction->setStatus(Transaction::STATUS_PAID);
+$transaction->setStatus(Status::PAID);
 
 var_dump($transaction);
