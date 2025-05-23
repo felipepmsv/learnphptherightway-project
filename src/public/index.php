97ebd6105391a00c885297d8ab0b70f861854e68
Felipe Santos <felipepmsv@gmail.com>
2025-05-23 17:17:54 -0300

    2.7 - Static Properties & Methods In Object Oriented PHP

--- src/public/index.php
+++ src/public/index.php
@@ -7,11 +7,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $transaction = new Transaction(25, 'Transaction 1');
 
-$db = DB::getInstance([]);
-$db = DB::getInstance([]);
-$db = DB::getInstance([]);
-$db = DB::getInstance([]);
-$db = DB::getInstance([]);
+var_dump($transaction::getCount());
+
+// Somente uma instancia de DB será criada
+// $db = DB::getInstance([]);
+// $db = DB::getInstance([]);
+// $db = DB::getInstance([]);
+// $db = DB::getInstance([]);
+// $db = DB::getInstance([]);
 
 //var_dump($transaction::process());
 //var_dump($transaction::$amount);
