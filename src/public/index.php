abc8c14ccb15af5760b5a013f07883b3437786ed
Felipe Santos <felipepmsv@gmail.com>
2025-05-23 17:49:33 -0300

    2.8 - Private or Protected doesn't mean much...!!!

--- src/public/index.php
+++ src/public/index.php
@@ -6,6 +6,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $transaction = new Transaction(25);
 
-//$transaction->amount = 50; // This will cause an error because $amount is private
+//$transaction->amount;  // propriedade privada, não pode ser acessada diretamente
+
+// Para acessar a propriedade privada, podemos usar ReflectionProperty
+$reflectionProperty = new ReflectionProperty(Transaction::class, 'amount');
+$reflectionProperty->setAccessible(true);
+var_dump($reflectionProperty->getValue($transaction));
+echo '<br>';
+
+// Agora o valor foi alterado
+$reflectionProperty->setValue($transaction, 50);
+var_dump($reflectionProperty->getValue($transaction));
+echo '<br>';
 
 $transaction->process();
