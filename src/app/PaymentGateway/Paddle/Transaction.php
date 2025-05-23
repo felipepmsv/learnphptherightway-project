5db77dce9c0ff907436e0b352e7a4019f6cb0d20
Felipe Santos <felipepmsv@gmail.com>
2025-05-23 17:38:49 -0300

    2.8 - Getters and Setters

--- src/app/PaymentGateway/Paddle/Transaction.php
+++ src/app/PaymentGateway/Paddle/Transaction.php
@@ -6,12 +6,26 @@ namespace App\PaymentGateway\Paddle;
 
 class Transaction
 {      
-    public float $amount;
+    private float $amount;
 
     public function __construct(float $amount)
     {
         $this->amount = $amount;        
     }
+
+    // Se não precisamos obter o valor do atributo, não precisamos de um getter
+    // já que o mesmo já foi passado no construtor
+    // public function getAmount(): float
+    // {
+    //     return $this->amount;
+    // }
+
+    // Nesse caso, se precisamos alterar o valor do atributo, 
+    // criamos uma nova instancia da classe e, portanto, não precisamos de um setter
+    // public function setAmount(float $amount): void
+    // {
+    //     $this->amount = $amount;
+    // }
 
     public function process()
     {
--- src/public/index.php
+++ src/public/index.php
@@ -6,4 +6,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $transaction = new Transaction(25);
 
+//$transaction->amount = 50; // This will cause an error because $amount is private
+
 $transaction->process();
