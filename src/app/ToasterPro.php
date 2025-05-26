1850c082443c667eab472d5b92d3b4fb163d7682
Felipe Santos <felipepmsv@gmail.com>
2025-05-26 15:24:02 -0300

    2.9 - Extends

---
+++ src/app/ToasterPro.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class ToasterPro extends Toaster
+{    
+    public int   $size = 4;
+
+    public function toastBagel()
+    {
+        foreach ($this->slices as $i => $slice) {
+            echo ($i + 1) . ': Toasting ' . $slice . ' with bagels option' . PHP_EOL;
+        }
+    }
+
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Toaster;
+use App\ToasterPro;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$toaster = new Toaster();
+$toaster = new ToasterPro();
 
 $toaster->addSlice('bread');
 $toaster->addSlice('bread');
