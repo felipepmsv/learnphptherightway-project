5d4dce4d2e5e0994707d88da91a8687b9f7c11af
Felipe Santos <felipepmsv@gmail.com>
2025-05-27 11:46:23 -0300

    2.10 - PHP Abstract Classes & Methods

---
+++ src/app/Boolean.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class Boolean extends Field
+{
+
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,16 +1,15 @@
 <?php
 
-use App\Toaster;
-use App\ToasterPro;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$toaster = new ToasterPro();
+$fields = [
+    new \App\Field('baseField'),
+    new \App\Text('textField'),
+    new \App\Boolean('booleanField'),
+    new \App\Checkbox('checkboxField'),
+    new \App\Radio('radioField'),
+];
 
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-$toaster->addSlice('bread');
-
-$toaster->toastBagel();
+foreach ($fields as $field) {
+    echo $field->render() . '<br />';
+}
