d3004d3205e84be37f020283c51685f3420c9655
Felipe Santos <felipepmsv@gmail.com>
2025-05-27 16:39:04 -0300

    2.11 - Before the crash

---
+++ src/app/DebtCollectionService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class DebtCollectionService
+{
+    public function collectDebt(CollectionAgency $collector)
+    {
+        $owedAmount = mt_rand(100, 1000); // Random amount between 100 and 1000
+        $collectedAmount = $collector->collect($owedAmount);
+
+        echo 'Collected $' . $collectedAmount . ' out of $' . $owedAmount . PHP_EOL;
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$collector = new \App\CollectionAgency();
+$service = new \App\DebtCollectionService();
 
-echo $collector->collect(100) . PHP_EOL;
+echo $service->collectDebt(new \App\CollectionAgency()) . PHP_EOL;
