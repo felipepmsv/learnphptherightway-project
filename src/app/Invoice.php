5bf7f391cba91b5c374ed36511680d2b5a254d8a
Felipe Santos <felipepmsv@gmail.com>
2025-05-29 10:51:22 -0300

    2.12 - DEBUGINFO

--- src/app/Invoice.php
+++ src/app/Invoice.php
@@ -6,14 +6,20 @@ namespace App;
 
 class Invoice
 {
-    public function __invoke()
+    private float $amount;
+    private int $id = 1;
+    private string $accountNumber = '123456789';
+
+    // This method is called by var_dump() when dumping an object 
+    // to get the properties that should be shown. 
+    // If the method isn't defined on an object, then all public, 
+    // protected and private properties will be shown.
+    
+    public function __debugInfo(): ?array
     {
-        // The __invoke() method is called when a script tries to call an object as a function.
-        // It's useful for creating callable objects.
-        // This method can be used to encapsulate functionality that you want to execute
-        // when the object is called like a function.
-        // For example, you might want to generate an invoice or perform some action related to invoicing.
-        
-        var_dump('invoked');
+        return [
+            'id' => $this->id,
+            'accountNumber' => '****' . substr($this->accountNumber, -4),
+        ];     
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $invoice = new App\Invoice();
 
-var_dump(is_callable($invoice)); // true
-
-$invoice();
+var_dump($invoice);
