1aab7e58a1619830d282beea6f3fabf5832bcf24
Felipe Santos <felipepmsv@gmail.com>
2025-05-30 16:55:57 -0300

    2.15 - PHP Anonymous Classes

--- src/public/index.php
+++ src/public/index.php
@@ -2,18 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$coffeeMaker = new \App\CoffeeMaker();
-$coffeeMaker->makeCoffee();
+$obj = new class {};
 
-$latteMaker = new \App\LatteMaker();
-$latteMaker->makeCoffee();
-$latteMaker->makeLatte();
-
-$cappuccinoMaker = new \App\CappuccinoMaker();
-$cappuccinoMaker->makeCoffee();
-$cappuccinoMaker->makeCappuccino();
-
-$allInOneCoffeeMaker = new \App\AllInOneCoffeeMaker();
-$allInOneCoffeeMaker->makeCoffee();
-$allInOneCoffeeMaker->makeLatte();
-$allInOneCoffeeMaker->makeCappuccino();
+var_dump($obj);
