903a0885892f01ccd7e0d2a464a0097091c761a8
Felipe Santos <felipepmsv@gmail.com>
2025-06-02 14:42:02 -0300

    2.16 - Circular Reference

--- src/app/Invoice.php
+++ src/app/Invoice.php
@@ -6,6 +6,8 @@ namespace App;
 
 class Invoice
 {
+    public ?Invoice $linkedInvoice = null;
+
     public function __construct(public Customer $customer, public float $amount, public string $description) 
     {
     }
--- src/public/index.php
+++ src/public/index.php
@@ -6,8 +6,12 @@ use App\Invoice;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $invoice1 = new Invoice(new \App\Customer('Customer 1'), 25, 'My Invoice');
-$invoice2 = new Invoice(new \App\Customer('Customer 2'), 25, 'My Invoice'); // false
-//$invoice2 = new Invoice(new \App\Customer('Customer 1'), 25, 'My Invoice'); //  true
+$invoice2 = new Invoice(new \App\Customer('Customer 2'), 25, 'My Invoice'); 
+
+// gerará erro de recursão infinita, 
+// por conta de referencia circular!
+$invoice1->linkedInvoice = $invoice2;
+$invoice2->linkedInvoice = $invoice1;
 
 echo 'invoice1 == invoice2' . PHP_EOL;
 var_dump($invoice1 == $invoice2);
@@ -15,7 +19,4 @@ var_dump($invoice1 == $invoice2);
 echo 'invoice1 === invoice2' . PHP_EOL;
 var_dump($invoice1 === $invoice2);
 
-// a primeira comparação verifica se os valores dos objetos são iguais, 
-// enquanto a segunda verifica se são o mesmo objeto na memória.
 var_dump($invoice1, $invoice2); 
-
