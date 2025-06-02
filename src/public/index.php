7747fb5344ca06903a769f75307181a0ebca268d
Felipe Santos <felipepmsv@gmail.com>
2025-06-02 18:03:32 -0300

    2.18 - Same Memory Location

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $invoice = new \App\Invoice();
-$invoice2 = new $invoice;
 
-var_dump($invoice, $invoice2, \App\Invoice::create());
+$invoice2 = $invoice; // ambos $invoice apontam para o mesmo objeto na memória
+
+
+var_dump($invoice, $invoice2, $invoice === $invoice2); // retorna true
