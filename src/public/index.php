b41ca56c5f495c18f2ce2524637e3f2ea411d899
Felipe Santos <felipepmsv@gmail.com>
2025-06-02 18:05:13 -0300

    2.18 - Clone

--- src/public/index.php
+++ src/public/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $invoice = new \App\Invoice();
 
-$invoice2 = $invoice; // ambos $invoice apontam para o mesmo objeto na memória
+$invoice2 = clone $invoice; // clona o objeto, criando uma nova instância
 
 
-var_dump($invoice, $invoice2, $invoice === $invoice2); // retorna true
+var_dump($invoice, $invoice2, $invoice === $invoice2); // retorna false
