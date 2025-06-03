04f1f8e5033a4d5c0c626f64f6f796bdec5389f0
Felipe Santos <felipepmsv@gmail.com>
2025-06-03 17:17:17 -0300

    2.20 - Try Catch

--- src/public/index.php
+++ src/public/index.php
@@ -7,4 +7,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $invoice = new Invoice(new Customer());
 
-$invoice->process(25);
+try 
+{
+    $invoice->process(25);
+} 
+catch (App\Exception\MissingBillingInfoException $e) 
+{
+    echo $e->getMessage() . ' ' . $e->getFile() . ':' . $e->getLine() . PHP_EOL;
+}
