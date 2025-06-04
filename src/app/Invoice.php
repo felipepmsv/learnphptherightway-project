f34c87ab9ae3dfdf89aeec5d27b6e06f2a96e8f3
Felipe Santos <felipepmsv@gmail.com>
2025-06-04 15:46:53 -0300

    2.22 - PHP Iterators & Iterable Type - Iterate Over Objects

--- src/app/Invoice.php
+++ src/app/Invoice.php
@@ -4,28 +4,13 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\Exception\InvoiceException;
-
 class Invoice
 {
-    public function __construct(public Customer $customer)
+    public int $id; // mudei de string para int por conta do uso de strict_types=1 !!!
+
+    public function __construct(public float $amount)
     {
+        $this->id = random_int(10000, 9999999);
     }
-
-    public function process(float $amount): void
-    {
-        if($amount <= 0) {
-            throw InvoiceException::invalidAmount();            
-        }
-
-        if(empty($this->customer->getBillingInfo())) {
-            throw InvoiceException::missingBillingInfo();            
-        }
-        
-        echo 'Processing $' . $amount . ' invoice - ';
-
-        sleep(1);
-
-        echo 'OK' . PHP_EOL;
-    }    
+    
 }
--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dateTime1 = new DateTime('2025-05-23 09:15:35 AM');
-$dateTime2 = new DateTime('2021-03-15 03:22:26 AM');
-
-echo $dateTime1->diff($dateTime2)->format('%Y years, %M months, %D days, %H hours, %I minutes, %S seconds') . "\n";
+foreach(new App\Invoice(25) as $key => $value) {
+    echo $key . ' => ' . $value . PHP_EOL;
+}
