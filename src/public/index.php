acb4bc9da707e8e7806df1d04725ea8731fff554
Felipe Santos <felipepmsv@gmail.com>
2025-06-04 15:32:48 -0300

    2.21 - DateTime Diff

--- src/public/index.php
+++ src/public/index.php
@@ -2,10 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dateTime = new DateTime('00:00');
+$dateTime1 = new DateTime('2025-05-23 09:15:35 AM');
+$dateTime2 = new DateTime('2021-03-15 03:22:26 AM');
 
-echo $dateTime->format('d/m/Y H:i') . ' - ' . $dateTime->getTimeZone()->getName() . PHP_EOL;
-
-$dateTime->setTimezone(new DateTimeZone('Europe/Lisbon'));
-
-echo $dateTime->format('d/m/Y H:i') . ' - ' . $dateTime->getTimeZone()->getName() . PHP_EOL;
+echo $dateTime1->diff($dateTime2)->format('%Y years, %M months, %D days, %H hours, %I minutes, %S seconds') . "\n";
