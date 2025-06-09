764031cad169337dafded52bccb230237f42c4bc
Felipe Santos <felipepmsv@gmail.com>
2025-06-09 14:54:17 -0300

    2.26 - Storage

--- src/app/Classes/Home.php
+++ src/app/Classes/Home.php
@@ -21,6 +21,27 @@ class Home
         echo '<pre>';
         var_dump($_FILES);
         echo '</pre>';
+
+        $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
+
+        // Para o uso desse comando foi necessario: 
+        // 1. criar a pasta storage
+        //    mkdir storage
+        // 2. descobrir o dono do servidor web
+        //    ps aux | grep php-fpm
+        // 3. dar permissão de escrita para o servidor web
+        //    sudo chown -R www-data:www-data storage
+        // 4. dar permissão de escrita a pasta storage
+        //    sudo chmod -R 755 storage
+
+        move_uploaded_file(
+            $_FILES['receipt']['tmp_name'],
+            $filePath            
+        );
+
+        echo '<pre>';
+        var_dump(pathinfo($filePath));
+        echo '</pre>';
     }
 
 }
--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
+define('STORAGE_PATH', __DIR__ . '/../storage/');
+
 $router = new \App\Router();
 
 $router
