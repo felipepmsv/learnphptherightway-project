34349e201eb9da565f0ae0ab69fcce7c7ddde1a2
Felipe Santos <felipepmsv@gmail.com>
2025-06-12 13:56:17 -0300

    2.28 - HTTP Headers In PHP - Request & Response Headers

--- src/public/index.php
+++ src/public/index.php
@@ -7,16 +7,24 @@ session_start();
 define('STORAGE_PATH', __DIR__ . '/../storage');
 define('VIEW_PATH', __DIR__ . '/../views');
 
-$router = new \App\Router();
+try
+{
+    $router = new \App\Router();
 
-$router
-    ->get('/', [App\Controllers\HomeController::class, 'index'])
-    ->post('/upload', [App\Controllers\HomeController::class, 'upload'])
-    ->get('/invoices', [App\Controllers\InvoiceController::class, 'index'])
-    ->get('/invoices/create', [App\Controllers\InvoiceController::class, 'create'])
-    ->post('/invoices/create', [App\Controllers\InvoiceController::class, 'store']);
+    $router
+        ->get('/', [App\Controllers\HomeController::class, 'index'])
+        ->post('/upload', [App\Controllers\HomeController::class, 'upload'])
+        ->get('/invoices', [App\Controllers\InvoiceController::class, 'index'])
+        ->get('/invoices/create', [App\Controllers\InvoiceController::class, 'create'])
+        ->post('/invoices/create', [App\Controllers\InvoiceController::class, 'store']);
 
-echo $router->resolve(
-    $_SERVER['REQUEST_URI'], 
-    strtolower($_SERVER['REQUEST_METHOD'])
-);
+    echo $router->resolve(
+        $_SERVER['REQUEST_URI'], 
+        strtolower($_SERVER['REQUEST_METHOD'])
+    );
+
+}
+catch (\App\Exception\RouteNotFoundException $e)
+{   
+    echo $e->getMessage();
+}
