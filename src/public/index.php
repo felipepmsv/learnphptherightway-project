d07c954cd4cabb2da6d176d695e1293fae17eda1
Felipe Santos <felipepmsv@gmail.com>
2025-06-12 14:04:38 -0300

    2.28 - Define o código de status HTTP 404 e Renderiza a view de erro 404

--- src/public/index.php
+++ src/public/index.php
@@ -26,5 +26,6 @@ try
 }
 catch (\App\Exception\RouteNotFoundException $e)
 {   
-    echo $e->getMessage();
+    header('HTTP/1.0 404 Not Found');  // Define o código de status HTTP 404
+    echo \App\View::make('error/404'); // Renderiza a view de erro 404   
 }
