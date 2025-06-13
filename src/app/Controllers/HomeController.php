f5a44e11f30b9057a1d330672e11ecce83f91a49
Felipe Santos <felipepmsv@gmail.com>
2025-06-13 10:33:33 -0300

    2.30 - PHP PDO Tutorial Part 1 - Prepared Statements - SQL Injection

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -10,43 +10,9 @@ class HomeController
 {
     public function index(): View
     {
-        return View::make('index', ['foo' => 'bar']);        
-    }
-
-    public function download()
-    {
-        header('Content-Type: application/pdf');
-        header('Content-Disposition: attachment; filename="banana.pdf"');
-
-        // Este comando é necessario para ler o arquivo e enviá-lo ao navegador
-        // por conta do buffer de saída do PHP!
-        // O arquivo deve existir na pasta storage
-        readfile(STORAGE_PATH . '/nome_do_arquivo_para_download.pdf'); 
+        phpinfo(); // Display PHP configuration information
         
-        // Este comando é necessário para evitar que 
-        // o código continue executando após o download
-        exit; 
-
-    }
-
-    public function upload()
-    {
-        // Deixei comantado para não executar o upload, 
-        // mas sim apenas testar o redirecionamento
-
-        // $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
-
-        // move_uploaded_file(
-        //     $_FILES['receipt']['tmp_name'],
-        //     $filePath            
-        // );
-
-        header('Location: /');
-
-        // Este comando é necessário para evitar que 
-        // o código continue executando 
-        // após o redirecionamento
-        exit; 
+        return View::make('index');
     }
 
 }
