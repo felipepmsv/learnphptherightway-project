8cbd20b3e80a30ec7f86c71b59b5bda846144fd2
Felipe Santos <felipepmsv@gmail.com>
2025-06-13 17:27:29 -0300

    2.30 - Prepared Statements

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -14,25 +14,36 @@ class HomeController
         try 
         {            
             $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', []);
-
-            // Criando uma situação de SQL Injection
-            // A URL de exemplo para testar a injeção SQL seria:
-            // http://localhost:8000/?email=foo@bar.com%22+OR+1=1+--+
-            $email = $_GET['email'];
-            $query = 'SELECT * FROM users WHERE email = "' . $email . '"';
-
-            echo $query . '<br><br>';
             
-            foreach($db->query($query) as $user)            
-            {
-                echo '<pre>';
-                var_dump($user);
-                echo '</pre>';
-            }                        
+            $email = 'joan@doe.com';
+            $name = 'Joan Doe';
+            $isActive = 1;
+            $createdAt = date('Y-m-d H:i:s', strtotime('2023-10-01 12:00:00'));
+
+            $query = 'INSERT INTO users (email, full_name, is_active, created_at) 
+            VALUES (:email, :name, :active, :date)';
+
+            $stmt = $db->prepare($query);
+
+            $stmt->bindValue(':name', $name);
+            $stmt->bindValue(':email', $email);
+            $stmt->bindValue(':active', $isActive, PDO::PARAM_BOOL);
+            $stmt->bindValue(':date', $createdAt);
+
+            $stmt->execute();
+
+            $id = (int) $db->lastInsertId();
+
+            $user = $db->query('SELECT * FROM users WHERE id = ' . $id)->fetch();
+            
+            echo '<pre>';
+            var_dump($user);
+            echo '</pre>';
+                                    
         } 
         catch (\PDOException $e) 
         {
-            throw new \PDOException($e->getMessage(), $e->getCode());
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
         }
 
         var_dump($db);
