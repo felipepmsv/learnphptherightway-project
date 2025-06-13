ebb7fd23db655fe02d118ae083bf75ef2ebf28ba
Felipe Santos <felipepmsv@gmail.com>
2025-06-13 16:00:40 -0300

    2.30 - fetchAll

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -15,13 +15,20 @@ class HomeController
         {
             // o nome do host é o mesmo do serviço criado no docker-compose.yml
             // referente ao banco de dados
-            $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', []);
+            $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', [                
+                // desta forma, o fetchAll retorna um array de objetos
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ                
+            ]);
 
             $query = 'SELECT * FROM users';
-
-            $stmt = $db->query($query);
-
-            var_dump($stmt->fetchAll());
+            
+            foreach($db->query($query) as $user)             
+            //foreach($db->query($query)->fetchAll(PDO::FETCH_OBJ) as $user) // retorna um array de objetos            
+            {
+                echo '<pre>';
+                var_dump($user);
+                echo '</pre>';
+            }                        
         } 
         catch (\PDOException $e) 
         {
