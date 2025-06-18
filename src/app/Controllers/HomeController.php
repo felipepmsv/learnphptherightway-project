da9479d160df70a1383a78f15c4adb43f879c11d
Felipe Santos <felipepmsv@gmail.com>
2025-06-18 16:49:46 -0300

    2.32 - Models & Refactoring

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Models\User;
+use App\Models\Invoice;
+
 use App\App;
 use App\View;
+use App\Models\SignUp;
 use PDO;
 
 class HomeController
@@ -13,66 +17,26 @@ class HomeController
     public function index(): View
     {
         // Obtém a instância do banco de dados
-        $db1 = App::db();
-        $db2 = App::db();
-        $db3 = App::db();
+        $db = App::db();
 
-        // Verifica se as instâncias são iguais
-        // Isso deve retornar true, pois todas as chamadas retornam a mesma instância        
-        var_dump($db1 === $db2, $db2 === $db3, $db1 === $db3);
-        exit;
-
-        $email = 'jaba@doe.com';
-        $name = 'Jaba Doe';
+        $email = 'joca@doe.com';
+        $name = 'Joca Doe';
         $amount = 25;
 
-        try 
-        {            
-            $db->beginTransaction();
+        $userModel = new User();
+        $invoiceModel = new Invoice();
 
-            $newUserStmt = $db->prepare(
-                'INSERT INTO users (email, full_name, is_active, created_at) 
-                VALUES (?, ?, 1, NOW())'
-            );
+        $invoiceId = (new SignUp($userModel, $invoiceModel))->register(
+            [
+                'email' => $email,
+                'name' => $name,                
+            ],
+            [
+                'amount' => $amount,
+            ]
+        );        
 
-            $newInvoiceStmt = $db->prepare(
-                'INSERT INTO invoices (amount, user_id) 
-                VALUES (?, ?)'
-            );
-            
-            $newUserStmt->execute([$email, $name]);
-
-            $userId = (int) $db->lastInsertId();
-
-            $newInvoiceStmt->execute([$amount, $userId]);
-
-            $db->commit();
-        }
-        catch (\Throwable $e) 
-        {
-            if($db->inTransaction())
-            {
-                $db->rollBack();
-            }            
-
-            throw $e;
-        }
-
-
-        $fetchStmt = $db->prepare(
-            'SELECT invoices.id AS invoice_id, amount, user_id, full_name
-             FROM invoices
-             INNER JOIN users ON user_id = users.id
-             WHERE email = ?'
-        );
-
-        $fetchStmt->execute([$email]);
-
-        echo '<pre>';
-        var_dump($fetchStmt->fetch(PDO::FETCH_ASSOC));
-        echo '</pre>';
-
-        return View::make('index');        
+        return View::make('index', ['invoice' => $invoiceModel->find($invoiceId)]);        
     }
 
 }
