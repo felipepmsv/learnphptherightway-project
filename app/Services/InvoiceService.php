e60634d93f75b234285a5b7055af2c6a08b02232
Felipe Santos <felipepmsv@gmail.com>
2025-07-10 15:49:31 -0300

    3.2 - Sem Test Doubles

--- app/Services/InvoiceService.php
+++ app/Services/InvoiceService.php
@@ -1,31 +1,29 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Services;
 
 class InvoiceService
 {
-    public function __construct(
-        protected SalesTaxService $salesTaxService,
-        protected PaymentGatewayService $gatewayService,
-        protected EmailService $emailService
-    ) {
-    }
-
     public function process(array $customer, float $amount): bool
     {
+        $salesTaxService = new SalesTaxService();
+        $gatewayService  = new PaymentGatewayService();
+        $emailService    = new EmailService();
+
         // 1. calculate sales tax
-        $tax = $this->salesTaxService->calculate($amount, $customer);
+        $tax = $salesTaxService->calculate($amount, $customer);
 
         // 2. process invoice
-        if (! $this->gatewayService->charge($customer, $amount, $tax)) {
+        if (! $gatewayService->charge($customer, $amount, $tax)) {
             return false;
         }
 
         // 3. send receipt
-        $this->emailService->send($customer, 'receipt');
+        $emailService->send($customer, 'receipt');
 
         return true;
     }
+
 }
