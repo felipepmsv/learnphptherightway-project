0c09d39cf93bf3967f56a4fda7fb291d240ae762
Felipe Santos <felipepmsv@gmail.com>
2025-08-05 17:27:02 -0300

    3.5 - DI Container With Interface Support (sugestão de leitura e exercício)

--- app/App.php
+++ app/App.php
@@ -5,8 +5,8 @@ declare(strict_types = 1);
 namespace App;
 
 use App\Exceptions\RouteNotFoundException;
-use App\Services\PaymentGatewayService;
-use App\Services\PaymentGatewayServiceInterface;
+use App\Services\PaymentGatewayInterface;
+use App\Services\PaddlePayment;
 use App\Services\StripePayment;
 
 class App
@@ -21,7 +21,8 @@ class App
     ) {
         static::$db = new DB($config->db ?? []);
 
-        $this->container->set(PaymentGatewayServiceInterface::class, StripePayment::class);
+        $this->container->set(PaymentGatewayInterface::class, PaddlePayment::class);
+        //$this->container->set(PaymentGatewayInterface::class, StripePayment::class);
     }
 
     public static function db(): DB
