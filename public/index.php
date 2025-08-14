fb1ae310b21d34ad3aa256677a702c20b7cf98d7
Felipe Santos <felipepmsv@gmail.com>
2025-08-14 15:44:19 -0300

    3.7 - Exemplo sem WeakMap

--- public/index.php
+++ public/index.php
@@ -7,9 +7,10 @@ use App\Invoice;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $invoice1 = new Invoice();
-$invoice2 = $invoice1;
+//$invoice2 = $invoice1;
 
 echo 'Unsetting Invoice 1' . PHP_EOL;
 unset($invoice1);
+echo 'Unset Invoice 1' . PHP_EOL;
 
-var_dump($invoice2);
+//var_dump($invoice2);
