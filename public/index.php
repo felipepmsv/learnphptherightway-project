b47d71c9aff2dd19907a6f738820624e8806d4d3
Felipe Santos <felipepmsv@gmail.com>
2025-08-15 15:52:43 -0300

    3.7 - Objeto coletado automaticamente

--- public/index.php
+++ public/index.php
@@ -7,10 +7,18 @@ use App\Invoice;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $invoice1 = new Invoice();
+$map = new WeakMap();
+
+$map[$invoice1] = ['a' => 1, 'b' => 2];
 //$invoice2 = $invoice1;
 
-echo 'Unsetting Invoice 1' . PHP_EOL;
+var_dump(count($map));
 unset($invoice1);
-echo 'Unset Invoice 1' . PHP_EOL;
+var_dump(count($map));
+
+var_dump($map);
+
+// echo 'Unsetting Invoice 1' . PHP_EOL;
+// echo 'Unset Invoice 1' . PHP_EOL;
 
 //var_dump($invoice2);
