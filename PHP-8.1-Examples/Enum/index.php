3dc4728b1092f911c459e47646f153f109b17f87
Felipe Santos <felipepmsv@gmail.com>
2025-08-18 17:15:14 -0300

    3.8 - What's New In PHP 8.1 - Deprecations & Backward Incompatible Changes

--- PHP-8.1-Examples/Enum/index.php
+++ PHP-8.1-Examples/Enum/index.php
@@ -1,13 +1,8 @@
 <?php
 
-declare(strict_types = 1);
+/*
 
-use PHP_8_1_Examples\Enum\Payment;
-use PHP_8_1_Examples\Enum\PaymentStatus;
+Muitas "putariazinhas" levantadas nesse modulo
+melhor reassistir caso tenha duvidas...
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-$payment = new Payment();
-$payment->updateStatus(PaymentStatus::PAID);
-
-echo $payment->status() . PHP_EOL;
+*/
